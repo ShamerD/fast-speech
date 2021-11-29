6352acbf86059b6fb24a780c85409bdb28084a8a
ShamerD <shomax31@gmail.com>
2021-11-29 22:08:46 +0300

    collator

--- src/utils/data/__init__.py
+++ src/utils/data/__init__.py
@@ -1,5 +1,11 @@
 from datasets import LJSpeechDataset
+from featurizer import MelSpectrogramConfig, MelSpectrogram
+from collator import Batch, LJSpeechCollator
 
 __all__ = [
-    "LJSpeechDataset"
+    "LJSpeechDataset",
+    "MelSpectrogramConfig",
+    "MelSpectrogram",
+    "Batch",
+    "LJSpeechCollator"
 ]
