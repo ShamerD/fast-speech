fc0ad9808eae9b0499f9767a5f8d01182d69cabc
ShamerD <shomax31@gmail.com>
2021-11-30 00:40:05 +0300

    fix module imports

--- src/utils/data/__init__.py
+++ src/utils/data/__init__.py
@@ -1,6 +1,6 @@
-from datasets import LJSpeechDataset
-from featurizer import MelSpectrogramConfig, MelSpectrogram
-from collator import Batch, LJSpeechCollator
+from .datasets import LJSpeechDataset
+from .featurizer import MelSpectrogramConfig, MelSpectrogram
+from .collator import Batch, LJSpeechCollator
 
 __all__ = [
     "LJSpeechDataset",
